00149e9fd7df36e96b9a46f6c6656089b1e1d829
Jeremy McGraw <jmcgraw159@fullsail.edu>
2014-04-01 18:05:04 -0400

    Fixed css error, added views to app.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,6 +9,18 @@ angular.module('isThatOutYetApp', [
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
+      .when('/detail', {
+        templateUrl: 'views/detail.html',
+        controller: 'DetailCtrl'
+      })
+      .when('/subscribe', {
+        templateUrl: 'views/subscribe.html',
+        controller: 'SubscribeCtrl'
+      })
+      .when('/unsubscribe', {
+        templateUrl: 'views/unsubscribe.html',
+        controller: 'UnsunscribeCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
