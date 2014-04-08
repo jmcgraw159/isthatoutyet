00310e6385f77f28b2f66440da13c40a9e088ed6
Jeremy McGraw <jmcgraw159@fullsail.edu>
2014-04-08 19:48:13 -0400

    Changed upcoming to latest. Added an API call to get the images of the latest games and display it in the HTML

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,11 +8,11 @@ App.controller('MainCtrl', function ($scope, $http) {
     'Karma'
   ];
 
-  // Try #1
-  $http.jsonp('http://www.giantbomb.com/api/search/?api_key=cdb456f4a15c4052a419f97b568218a2b50634c9&format=jsonp&json_callback=angular.callbacks._0&query=gameTitle&resources=game')
+  // API call to get the images for the latest releases
+  $http.jsonp('http://www.giantbomb.com/api/games/?api_key=cdb456f4a15c4052a419f97b568218a2b50634c9&filter=original_release_date%3A1700-01-01%2000%3A00%3A00%7C2014-04-08%2000%3A00%3A00&format=jsonp&callback=JSON_CALLBACK&json_callback=angular.callbacks._0&limit=5&sort=original_release_date%3Adesc')
     .success(function(data){
-      console.log(data);
-
+      $scope.upcoming = data;
+      console.log($scope.upcoming.results[0].image.thumb_url);
     })
     .error(function(data) {
       console.log(data);
@@ -20,20 +20,24 @@ App.controller('MainCtrl', function ($scope, $http) {
 
 });
 
+// Function used to create a drop-down list of related items to what is typed in by the user
 function TypeaheadCtrl($scope, $http) {
   $scope.selected = undefined;
-  // Any function returning a promise object can be used to load values asynchronously
+
+  // Trying to update callback so it don't change the number
+  var callback = 'angular.callbacks._1';
+
   $scope.getGame = function(val) {
-    return $http.jsonp('http://www.giantbomb.com/api/search/?api_key=cdb456f4a15c4052a419f97b568218a2b50634c9&format=jsonp&json_callback=angular.callbacks._0&resources=game', {
+    return $http.jsonp('http://www.giantbomb.com/api/search/?api_key=cdb456f4a15c4052a419f97b568218a2b50634c9&format=jsonp&callback=' + callback + '&json_callback=angular.callbacks._1&resources=game&limit=10', {
       params: {
         query: val
       }
     }).then(function(res){
       var game = [];
       angular.forEach(res.data.results, function(item){
+        console.log(item.name);
         game.push(item.name);
       });
-      console.log(res);
       return game;
     });
   };
