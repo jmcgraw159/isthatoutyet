5920ae6468ee8ef7625d20d39eab220edeba497f
Jeremy McGraw <jmcgraw159@fullsail.edu>
2014-04-10 18:51:32 -0400

    Added API call to get specific game details based on the  id

--- app/scripts/controllers/detail.js
+++ app/scripts/controllers/detail.js
@@ -1,6 +1,21 @@
 'use strict';
 
 var App = angular.module('isThatOutYetApp');
-  App.controller('DetailCtrl', function ($scope) {
+App.controller('DetailCtrl', function ($scope, $http, $window, $routeParams) {
 
-  });
+  // API call to get game detail
+  $http.jsonp('http://www.giantbomb.com/api/games/?api_key=cdb456f4a15c4052a419f97b568218a2b50634c9&format=jsonp', {
+	   params: {
+	        callback: 'JSON_CALLBACK',
+	        json_callback: 'angular.callbacks._' + $window.angular.callbacks.counter.toString(36),
+	        filter: 'id' + $routeParams.id
+	    }
+    })
+    .success(function(data){
+      console.log(data);
+    })
+    .error(function(data) {
+      console.log(data);
+    });
+
+});
