81182342d482d75392f0bfa6afbb06e2f351d3cb
Jeremy McGraw <jmcgraw159@fullsail.edu>
2014-04-13 13:35:26 -0400

    Recent releases are now shown based on the current date

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,27 +1,31 @@
 'use strict';
 
 var App = angular.module('isThatOutYetApp');
-App.controller('MainCtrl', function ($scope, $http, $window) {
+App.controller('MainCtrl', function ($scope, $http, $window, $filter) {
   $scope.awesomeThings = [
     'HTML5 Boilerplate',
     'AngularJS',
     'Karma'
   ];
 
+  // Retrive today's date
+  var today = $filter('date')(new Date(),'yyyy-MM-dd');
+
   // API call to get the images for the latest releases
-  $http.jsonp('http://www.giantbomb.com/api/games/?api_key=cdb456f4a15c4052a419f97b568218a2b50634c9&format=jsonp&filter=original_release_date%3A1700-01-01%2000%3A00%3A00%7C2014-04-08%2000%3A00%3A00&limit=5&sort=original_release_date%3Adesc', {
+  $http.jsonp('http://www.giantbomb.com/api/games/?api_key=cdb456f4a15c4052a419f97b568218a2b50634c9&format=jsonp&limit=5&sort=original_release_date%3Adesc', {
     params: {
         callback: 'JSON_CALLBACK',
         json_callback: 'angular.callbacks._' + $window.angular.callbacks.counter.toString(36),
+        filter: 'original_release_date:1700-01-01|' + today
       }
     })
     .success(function(data){
       $scope.upcoming = data;
 
-      // Check to see if there is an image for the game or if the image didn't load
+      // Check to see if there if the image is glitching
       angular.forEach(data.results, function(item){
         if(item.image) {
-          console.log(item.image.small_url);
+          // console.log(item.image.small_url);
         }else  {
           console.log('No Image');
         }
