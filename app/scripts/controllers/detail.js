e608003eb553e4924c3d5e31c7f52edc50239dc7
Jeremy McGraw <jmcgraw159@fullsail.edu>
2014-04-14 19:27:26 -0400

    Email signup now also adds the game title and release date to the database

--- app/scripts/controllers/detail.js
+++ app/scripts/controllers/detail.js
@@ -2,7 +2,7 @@
 
 var App = angular.module('isThatOutYetApp');
 
-App.controller('DetailCtrl', function ($scope, $http, $window, $routeParams) {
+App.controller('DetailCtrl', function ($scope, $http, $window, $routeParams, $rootScope) {
 
   // API call to get game detail
   $http.jsonp('http://www.giantbomb.com/api/games/?api_key=cdb456f4a15c4052a419f97b568218a2b50634c9&format=jsonp', {
@@ -15,18 +15,26 @@ App.controller('DetailCtrl', function ($scope, $http, $window, $routeParams) {
   .success(function(data){
       $scope.details = data.results[0];
 
+      $rootScope.name = $scope.details.name;
+
       // Condition to check what date format should be shown
       if(data.results[0].expected_release_day === null && data.results[0].expected_release_month !== null && data.results[0].expected_release_year !== null){
 
         $scope.date = data.results[0].expected_release_month + '/' + data.results[0].expected_release_year;
 
+        $rootScope.date = $scope.date;
+
       }else if(data.results[0].expected_release_day === null && data.results[0].expected_release_month === null && data.results[0].expected_release_year !== null) {
 
         $scope.date = data.results[0].expected_release_year;
 
+        $rootScope.date = $scope.date;
+
       }else if(data.results[0].expected_release_day !== null && data.results[0].expected_release_month !== null && data.results[0].expected_release_year !== null) {
 
         $scope.date = data.results[0].expected_release_month + '/' + data.results[0].expected_release_day + '/' + data.results[0].expected_release_year;
+
+        $rootScope.date = $scope.date;
 
       }else {
 
@@ -51,7 +59,7 @@ App.controller('DetailCtrl', function ($scope, $http, $window, $routeParams) {
 
 });
 
-App.controller('EmailCtrl', ['$scope', '$firebase', function ($scope, $firebase) {
+App.controller('EmailCtrl', ['$scope', '$firebase', '$rootScope', function ($scope, $firebase, $rootScope) {
 
   // console.log($firebase);
 
@@ -60,7 +68,7 @@ App.controller('EmailCtrl', ['$scope', '$firebase', function ($scope, $firebase)
   $scope.addData = function()  {
     console.log('Run');
 
-    $scope.email.$add({email: $scope.email.inputEmail, selectedDate: $scope.email.selectedDate});
+    $scope.email.$add({email: $scope.email.inputEmail, title: $rootScope.name, date: $rootScope.date, selectedDate: $scope.email.selectedDate});
   }
 
 }])
