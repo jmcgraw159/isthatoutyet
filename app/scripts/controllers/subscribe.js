2e0f01dd985c010901825dba0643a19ffa48ddad
Jeremy McGraw <jmcgraw159@fullsail.edu>
2014-04-26 12:49:05 -0400

    If the subscribe page receives a null value, it will redirect to homepage. Fix issue with recent release

--- app/scripts/controllers/subscribe.js
+++ app/scripts/controllers/subscribe.js
@@ -1,51 +1,53 @@
 'use strict';
 
 var App = angular.module('isThatOutYetApp');
-App.controller('SubscribeCtrl', function ($scope, $routeParams) {
+App.controller('SubscribeCtrl', function ($scope, $routeParams, $window) {
 
-	// Firebase URLs
-	$scope.url = new Firebase('https://isthatoutyet.firebaseio.com/unconfirmed/' + $routeParams.email + '/' + $routeParams.id);
-	$scope.unconfirmed = new Firebase('https://isthatoutyet.firebaseio.com/unconfirmed/' + $routeParams.email);
-	$scope.confirmed = new Firebase('https://isthatoutyet.firebaseio.com/confirmed/' + $routeParams.email);
+  // Firebase URLs
+  $scope.url = new Firebase('https://isthatoutyet.firebaseio.com/unconfirmed/' + $routeParams.email + '/' + $routeParams.id);
+  $scope.unconfirmed = new Firebase('https://isthatoutyet.firebaseio.com/unconfirmed/' + $routeParams.email);
+  $scope.confirmed = new Firebase('https://isthatoutyet.firebaseio.com/confirmed/' + $routeParams.email);
 
-	$scope.url.once('value', function(ss) {
+  $scope.url.once('value', function(ss) {
 
-		// Check to see if value exists
-        if(ss.val() === null) {
+    // Check to see if value exists
+    if(ss.val() === null) {
 
-        	console.log('null');
+      console.log('null');
+
+      // If null, redirect to homepage
+      $window.location = '/';
+
+    }else {
+
+      ss.forEach(function(data) {
+
+        var checker = data.val();
+
+        // Check to make sure value is false
+        if(checker.confirmed === false) {
+
+          // If false, set to true
+          $scope.url.child(data.name()).set({confirmed: 'true'}, function() {
+
+            $scope.unconfirmed.on('value', function(screenshot) {
+
+              var data = screenshot.val();
+
+              // Push data to confirm section
+              $scope.confirmed.push(data);
+
+              // Remove data from unconfirmed section
+              $scope.unconfirmed.remove();
+            })
+
+          });
 
         }else {
-
-	    	ss.forEach(function(data) {
-
-	    		var checker = data.val();
-
-	    		// Check to make sure value is false
-	    		if(checker.confirmed === false) {
-
-	    			// If false, set to true
-	    			$scope.url.child(data.name()).set({confirmed: 'true'}, function() {
-
-		    			$scope.unconfirmed.on('value', function(screenshot) {
-
-		    				var data = screenshot.val();
-
-		    				// Push data to confirm section
-		    				$scope.confirmed.push(data);
-
-		    				// Remove data from unconfirmed section
-		    				$scope.unconfirmed.remove();
-		    			})
-
-		    		});
-
-	    		}else {
-	    			console.log(checker.confirmed)
-	    		}
-
-	    	});
+          console.log(checker.confirmed)
         }
-    });
 
+      });
+    }
+  });
 });
