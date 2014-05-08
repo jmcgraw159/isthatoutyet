d4b22db12da68561dfe1ac7f789000a17fe13ea1
Jeremy McGraw <jmcgraw159@fullsail.edu>
2014-05-08 18:57:35 -0400

    Server

--- api/app/config/database.php
+++ api/app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'isthatoutyet',
 			'username'  => 'root',
-			'password'  => 'root',
+			'password'  => 'bw2vF2',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
