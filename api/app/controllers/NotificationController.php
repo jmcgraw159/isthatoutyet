2bf8c21a92a82106513843a9d4c8bf1e831cb660
Jeremy McGraw <jmcgraw159@fullsail.edu>
2014-05-11 19:30:36 -0400

    Testing month format

--- api/app/controllers/NotificationController.php
+++ api/app/controllers/NotificationController.php
@@ -7,15 +7,15 @@ class NotificationController extends BaseController {
 	public static function sendNotification()
 	{
 		// Get current date
-		$month = date('m');
+		$month = date('mm');
 		$day = date('d');
 		$year = date('Y');
 
-		echo $month . $day . $year;
+		echo $month;
 
 		$count = UsersGames::where('month', '=', $month, 'and', 'day', '=', $day, 'and', 'year', '=', $year)
 								->count();
 
-		echo $count;
+		echo 'Count is ' . $count;
 	}
 }
