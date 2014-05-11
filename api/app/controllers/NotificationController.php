cb5432f3d006c8b9ee345e08e93443e0f9c2de81
Jeremy McGraw <jmcgraw159@fullsail.edu>
2014-05-11 19:26:08 -0400

    Trying another way

--- api/app/controllers/NotificationController.php
+++ api/app/controllers/NotificationController.php
@@ -7,11 +7,13 @@ class NotificationController extends BaseController {
 	public static function sendNotification()
 	{
 		// Get current date
-		$date = date('mdY');
+		$month = date('m');
+		$day = date('d');
+		$year = date('Y');
 
 		echo $date;
 
-		$count = UsersGames::where(UsersGames::raw('concat(month,day,year)'), '=', $date)
+		$count = UsersGames::where('month', '=', $month, 'and', 'day', '=', $day, 'and', 'year', '=', $year)
 								->count();
 
 		echo $count;
