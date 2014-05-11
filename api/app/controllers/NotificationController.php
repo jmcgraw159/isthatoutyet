8a2cb871f6195574b2f1443a2d5fe7b5e3a00c09
Jeremy McGraw <jmcgraw159@fullsail.edu>
2014-05-11 19:51:54 -0400

    Testing count

--- api/app/controllers/NotificationController.php
+++ api/app/controllers/NotificationController.php
@@ -16,6 +16,6 @@ class NotificationController extends BaseController {
 		$count = UsersGames::where('month', '=', $month, 'and', 'day', '+', 'selected_date', '=', $day, 'and', 'year', '=', $year)
 								->count();
 
-		echo 'Count is ' . $count[0]->id;
+		echo 'Count is ' . $count;
 	}
 }
