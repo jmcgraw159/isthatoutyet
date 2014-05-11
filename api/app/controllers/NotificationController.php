7f961bf96fb9a4e0f09b43de8d62909835073ce4
Jeremy McGraw <jmcgraw159@fullsail.edu>
2014-05-11 19:48:22 -0400

    Find game based on current date plus the selected day

--- api/app/controllers/NotificationController.php
+++ api/app/controllers/NotificationController.php
@@ -13,9 +13,9 @@ class NotificationController extends BaseController {
 
 		echo $month . $day . $year;
 
-		$count = UsersGames::where('month', '=', $month, 'and', 'day', '=', $day, 'and', 'year', '=', $year)
+		$count = UsersGames::where('month', '=', $month, 'and', 'day', '+', 'selected_date' '=', $day, 'and', 'year', '=', $year)
 								->count();
 
-		echo 'Count is ' . $count;
+		echo 'Count is ' . $count[0]->id;
 	}
 }
