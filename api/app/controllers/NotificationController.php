fa051b4898b021ae74cf3fc50e40fb781a3d4910
Jeremy McGraw <jmcgraw159@fullsail.edu>
2014-05-11 20:45:34 -0400

    Trying to figure out why I'm getting more results than I should

--- api/app/controllers/NotificationController.php
+++ api/app/controllers/NotificationController.php
@@ -18,7 +18,7 @@ class NotificationController extends BaseController {
 		echo $month . $day . $year;
 
 		// Select games that match the current date
-		$getContent = UsersGames::where('month', '=', $month, 'and', DB::raw('day + selected_date'), '=', $day, 'and', 'year', '=', $year)
+		$getContent = UsersGames::where('month', '=', $month, 'and', DB::raw('day + 0'), '=', $day, 'and', 'year', '=', $year)
 								->get();
 
 		foreach($getContent as $game) {
