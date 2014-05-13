dd1f9d4d843a1220d2f2e6578dde4d943321d9f2
Jeremy McGraw <jmcgraw159@fullsail.edu>
2014-05-13 18:27:24 -0400

    Implementing the Mandrill API to send Notification email

--- api/app/controllers/NotificationController.php
+++ api/app/controllers/NotificationController.php
@@ -38,12 +38,12 @@ class NotificationController extends BaseController {
 			$data = array('email' => $game->email, 'title' => $game->title, 'selected' => $selected);
 
 			// Send mail
-			// Mail::send('emails.notification', $data, function($message) use($data)
-			// {
-			//     $message
-			//     ->to($data['email'])
-			//     ->subject('Game Notification');
-			// });
+			Mail::send('emails.notification', $data, function($message)
+			{
+			    $message
+			    ->to($data['email'])
+			    ->subject('Game Notification');
+			});
 
 		}
 
