55f519222a929345b23c596ae5303da74d745d9a
Jeremy McGraw <jmcgraw159@fullsail.edu>
2014-05-13 18:34:25 -0400

    Testing new user email.

--- api/public/scripts/controllers/detail.js
+++ api/public/scripts/controllers/detail.js
@@ -112,7 +112,7 @@ App.controller('EmailCtrl', ['$scope', '$rootScope', '$http', function ($scope, 
 
         console.log(data);
 
-        if(data === '0' || data === 0) {
+        if(data === '00' || data === 00) {
           // Push alert to array to display
           $scope.alerts.push({type: 'success', msg: "Success! We will send you an email to confirm your email address."});
         }else {
