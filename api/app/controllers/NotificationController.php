77aa1f1ab26665566e7963f1729557319cae6e18
Jeremy McGraw <jmcgraw159@fullsail.edu>
2014-05-13 17:43:46 -0400

    Implementing the Mandrill API to send Notification email

--- api/app/controllers/NotificationController.php
+++ api/app/controllers/NotificationController.php
@@ -25,13 +25,13 @@ class NotificationController extends BaseController {
 		// If date is = current date
 		foreach($getContent as $game) {
 
-			echo $game.title;
+			echo $game->selected_date;
 
-			if($game.selected_date === '0') {
+			if($game->selected_date === '0') {
 				$selected = 'on the day';
-			}else if($game.selected_date === '1') {
+			}else if($game->selected_date === '1') {
 				$selected = '1 day before';
-			}else if($game.selected_date === '2') {
+			}else if($game->selected_date === '2') {
 				$selected === '2 days before';
 			}else  {
 				$selected === '3 days before';
