7e450a023aedad580a52ad05bbbaea47351a88f5
Jeremy McGraw <jmcgraw159@fullsail.edu>
2014-05-14 13:50:28 -0400

    Trying to fix MySQL returned data

--- api/app/controllers/NotificationController.php
+++ api/app/controllers/NotificationController.php
@@ -18,10 +18,8 @@ class NotificationController extends BaseController {
 		// echo $month . $day . $year;
 
 		// Select games that match the current date
-		$getContent = DB::select(DB::raw('select * from users_games where day + selected_date = :date and month = :month and year = :year'), array(
-											'date' => $day,
-											'month' => $month,
-											'year' => $year));
+		$getContent = DB::select(DB::raw('select * from users_games where day + selected_date = :date'), array(
+											'date' => $day,));
 		echo $getContent;
 		// If date is = current date
 		// foreach($getContent as $game) {
