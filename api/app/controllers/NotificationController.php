47891e582cf8417508176a375c8246b3611b1436
Jeremy McGraw <jmcgraw159@fullsail.edu>
2014-05-14 14:45:40 -0400

    Changed plus to minus for MySQL statement. Added condition to check to make sure the email was confirmed.

--- api/app/controllers/NotificationController.php
+++ api/app/controllers/NotificationController.php
@@ -19,7 +19,7 @@ class NotificationController extends BaseController {
 
 		// Select games that match the current date
 		$getContent = UsersGames::where('month', '=', $month)
-								->where(DB::raw('day + selected_date'), '=', $day)
+								->where(DB::raw('day - selected_date'), '=', $day)
 								->where('year', '=', $year)
 								->join('users', 'user_id', '=', 'users.id')
 								->get();
@@ -47,16 +47,19 @@ class NotificationController extends BaseController {
 
 			}
 
-			// Info to send to Mandrill API
-			$data = array('email' => $game->email, 'title' => $game->title, 'selected' => $selected, 'id' => $game->user_id);
+			// If the email has been confirmed
+			if($game->confirmed === '1') {
+				// Info to send to Mandrill API
+				$data = array('email' => $game->email, 'title' => $game->title, 'selected' => $selected, 'id' => $game->user_id);
 
-			// Send mail
-			Mail::send('emails.notification', $data, function($message) use($data)
-			{
-			    $message
-			    ->to($data['email'])
-			    ->subject('Game Notification');
-			});
+				// Send mail
+				Mail::send('emails.notification', $data, function($message) use($data)
+				{
+				    $message
+				    ->to($data['email'])
+				    ->subject('Game Notification');
+				});
+			}
 
 		}
 
