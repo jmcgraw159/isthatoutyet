0fb8bc9029acbc2f3510ef95484d7885f195969a
Jeremy McGraw <jmcgraw159@fullsail.edu>
2014-05-15 17:48:31 -0400

    Testing Supervisor

---
+++ api/app/models/MyQueue.php
@@ -0,0 +1,63 @@
+<?php
+
+class MyQueue{
+    public function fire($job, $data){
+
+    	// Set timezone
+		date_default_timezone_set('EST');
+
+		// Get current date
+		$month = date('n');
+		$day = date('j');
+		$year = date('Y');
+
+		// echo $month . $day . $year;
+
+		// Select games that match the current date
+		$getContent = UsersGames::where('month', '=', $month)
+								->where(DB::raw('day - selected_date'), '=', $day)
+								->where('year', '=', $year)
+								->join('users', 'user_id', '=', 'users.id')
+								->get();
+
+		// If date is = current date
+		foreach($getContent as $game) {
+
+			if($game->selected_date === '0') {
+
+				$selected = 'on the day';
+
+			}elseif($game->selected_date === '1') {
+
+				$selected = '1 day before';
+
+			}elseif($game->selected_date === '2') {
+
+				$selected === '2 days before';
+
+			}else  {
+
+				$selected === '3 days before';
+
+			}
+
+			// If the email has been confirmed
+			if($game->confirmed === '1') {
+				echo $game;
+
+				// Info to send to Mandrill API
+				$data = array('email' => $game->email, 'title' => $game->title, 'selected' => $selected, 'id' => $game->user_id);
+
+				// Send mail
+				Mail::send('emails.notification', $data, function($message) use($data)
+				{
+				    $message
+				    ->to($data['email'])
+				    ->subject('Game Notification');
+				});
+			}
+		}
+
+        $job->delete();
+    }
+}
