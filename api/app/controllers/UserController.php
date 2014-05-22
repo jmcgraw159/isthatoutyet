9fdbe532f009db0d4dcc8c5cb9fbcc4814b896dc
Jeremy McGraw <jmcgraw159@fullsail.edu>
2014-05-21 20:37:25 -0400

    URL test

--- api/app/controllers/UserController.php
+++ api/app/controllers/UserController.php
@@ -25,7 +25,7 @@ class UserController extends BaseController {
 
 			$userId = $getUser[0]->id;
 
-			$url = urldecode($game_cover);
+			$url = urlencode($game_cover);
 
 			$insertGames = UsersGames::insert(array(
 				'user_id' => $userId,
@@ -56,7 +56,7 @@ class UserController extends BaseController {
 
 			$userId = $getUser[0]->id;
 
-			$url = urldecode($game_cover);
+			$url = urlencode($game_cover);
 
 			$insertGames = UsersGames::insert(array(
 				'user_id' => $userId,
