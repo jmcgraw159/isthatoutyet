05678b2e2e4ff37e1a265eeadf7bbd6eb7cc67f5
Jeremy McGraw <jmcgraw159@fullsail.edu>
2014-05-23 12:59:39 -0400

    Trying to fix glitch where recent releases image is blank

--- api/public/scripts/controllers/main.js
+++ api/public/scripts/controllers/main.js
@@ -23,6 +23,7 @@ App.controller('MainCtrl', function ($scope, $http, $filter) {
           // console.log(item.image.small_url);
         }else  {
           console.log('No Image');
+          item.image.small_url = 'images/image_error.jpg';
         }
       });
 
