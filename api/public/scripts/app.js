f20be414a9255171d91e9a25323c471b3507ecc7
Jeremy McGraw <jmcgraw159@fullsail.edu>
2014-05-27 19:20:03 -0400

    Working on making the search results faster

--- api/public/scripts/app.js
+++ api/public/scripts/app.js
@@ -40,29 +40,29 @@ App.controller('TypeaheadCtrl', ['$scope', '$http', '$routeParams', '$rootScope'
 
       var game = [];
 
-      console.log(res);
+      // console.log(res);
 
-      if(res.data.count >= 1) {
-        console.log('Database');
+      // if(res.data.count >= 1) {
+      //   console.log('Database');
 
-        angular.forEach(res.data, function(item){
+      //   angular.forEach(res.data, function(item){
 
-          console.log(item);
+      //     console.log(item);
 
-          // game.push({name: item.title, id: item.id, image: item.image});
-        });
+      //     // game.push({name: item.title, id: item.id, image: item.image});
+      //   });
 
-        return game;
+      //   return game;
 
-      }else {
-        console.log('API');
+      //}else {
+        //console.log('API');
 
         angular.forEach(res.data.results, function(item){
           game.push({name: item.name, id: item.id, image: item.image});
         });
 
         return game;
-      }
+      //}
     });
   };
 
