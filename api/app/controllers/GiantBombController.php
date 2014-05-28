5e70da010169eb00d28ba623ac5c746405218b9e
Jeremy McGraw <jmcgraw159@fullsail.edu>
2014-05-28 12:12:56 -0400

    Working on making the search results faster

--- api/app/controllers/GiantBombController.php
+++ api/app/controllers/GiantBombController.php
@@ -37,10 +37,10 @@ class GiantBombController extends BaseController {
 			return Response::json($response);
 		}else  {
 
-			$getGame = DB::select(DB::raw('select * from games where title like :title'), array(
-				'title' => $title));
+			$getGame = DB::select(DB::raw('select * from games where title like :game'), array(
+				'game' => $title));
 
-			return $getGame->toArray();
+			return $getGame;
 
 			// $selectGame = $getGame->first();
 
