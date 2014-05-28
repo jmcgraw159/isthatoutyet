2d11306611b99c456832a65815a81187d21aa066
Jeremy McGraw <jmcgraw159@fullsail.edu>
2014-05-28 13:53:57 -0400

    Setting up condition to added game to database for searching later

--- api/app/controllers/GiantBombController.php
+++ api/app/controllers/GiantBombController.php
@@ -33,11 +33,11 @@ class GiantBombController extends BaseController {
 
 			$response = json_decode($call);
 
-			echo $response['results'];
+			// echo $response['results'];
 
-			// foreach ($response as $item) {
-			// 	echo $item;
-			// }
+			foreach ($call['results'] as $item) {
+				echo $item;
+			}
 
 			// $insertGame = Games::insert(array(
 			// 	'game_id' => $game_id,
