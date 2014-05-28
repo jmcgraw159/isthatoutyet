2e39d738e6dfd002fe36cff320dfa0138206067d
Jeremy McGraw <jmcgraw159@fullsail.edu>
2014-05-28 14:51:13 -0400

    Trying to fix image issue

--- api/public/scripts/app.js
+++ api/public/scripts/app.js
@@ -56,7 +56,7 @@ App.controller('TypeaheadCtrl', ['$scope', '$http', '$routeParams', '$rootScope'
           if(item.image.small_url === null) {
             $scope.imageFix = '../../images/image_error.jpg';
           }else  {
-            $scope.imageFix = $item.image.small_url;
+            $scope.imageFix = item.image.small_url;
           }
 
           game.push({name: item.name, id: item.id, image: imageFix, release_day: item.expected_release_day, release_month: item.expected_release_month, release_year: item.expected_release_year, release_date: item.original_release_date, desc: item.deck, platforms: item.platforms});
